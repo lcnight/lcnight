24541ffde741b8b5cda4db074365e1dcfb0bf892
lc <lcnight2012@gmail.com>
2012-04-05 16:37:23 +0800

    add unique phrase mapred, nice sync files and run command

---
+++ hadoop/mapred/uniq-clause-load-db/GetUniqClause.java
@@ -0,0 +1,265 @@
+import java.io.IOException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.util.*;
+        
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+        
+public class GetUniqClause extends Configured implements Tool {
+        
+    public static class TextPair implements WritableComparable<TextPair> {
+        private Text first;
+        private Text second;
+
+        public TextPair() {
+            set(new Text(), new Text());
+        }
+        public TextPair(Text f, Text s) {
+            set(f, s);
+        }
+        public void set(Text f, Text s) {
+            this.first = f;
+            this.second = s;
+        }
+        public void set(String f, String s) {
+            this.first = new Text(f);
+            this.second = new Text(s);
+        }
+
+        public Text getFirst() {
+            return first;
+        }
+        public Text getSecond() {
+            return second;
+        }
+
+        @Override
+        public void write(DataOutput out) throws IOException {
+            first.write(out);
+            second.write(out);
+        }
+
+        @Override
+        public void readFields(DataInput in) throws IOException {
+            first.readFields(in);
+            second.readFields(in);
+        }
+
+        @Override
+        public int hashCode() {
+            return first.hashCode() * 163 + second.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof TextPair) {
+                TextPair tp = (TextPair) o;
+                return first.equals(tp.first) && second.equals(tp.second);
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return first + "\t" + second;
+        }
+
+        @Override
+        public int compareTo(TextPair tp) {
+            int ret = first.compareTo(tp.first);
+            if (ret != 0) {
+                return ret;
+            }
+            return second.compareTo(tp.second);
+        }
+
+        static {  // register comparator
+            WritableComparator.define(TextPair.class, new TextPairComparator());
+        }
+    }
+
+    public static class TextPairComparator extends WritableComparator
+    {
+        private static final Text.Comparator CMP = new Text.Comparator();
+        public TextPairComparator() {
+            super(TextPair.class);
+        }
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+            try { 
+                int fl1 = WritableUtils.decodeVIntSize(b1[s1]) + readVInt(b1, s1);
+                int fl2 = WritableUtils.decodeVIntSize(b2[s2]) + readVInt(b2, s2);
+                int cmp = CMP.compare(b1, s1, fl1, b2, s2, fl2);
+                if (cmp != 0) {
+                    return cmp;
+                }
+
+                return CMP.compare(b1, s1 + fl1, l1 - fl1, b2, s2 + fl2, l2 - fl2);
+
+            } catch (IOException e) {
+                System.err.println("compare error for TextPair");
+                throw new IllegalArgumentException(e);
+            }
+        }
+
+        @Override
+        public int compare(WritableComparable o1, WritableComparable o2) {
+            return o1.compareTo(o2);
+        }
+    }
+
+    //public static class MyGroupingComparator implements RawComparator<TextPair> {
+        //@Override
+        //public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+            //try { 
+            //} catch (IOException e) {
+            //}
+        //}
+
+        //@Override
+        //public int compare(TextPair o1, TextPair o2) {
+            //return o1.compareTo(o2);
+        //}
+    //}
+
+    public static class TextPairPartitioner extends Partitioner<TextPair,IntWritable>
+    {
+        @Override
+        public int getPartition(TextPair key, IntWritable value, int numPartitions) {
+            return Math.abs(key.getFirst().hashCode()) % numPartitions;
+        }
+    }
+
+ public static class Map extends Mapper<LongWritable, Text, TextPair, IntWritable> 
+ {
+    private final static IntWritable one = new IntWritable(1);
+    private TextPair word = new TextPair();
+        
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        int beg = line.indexOf(']');
+        line = line.substring(beg + 1);
+        String[] clause = line.split(":");
+
+        if (clause.length < 15) {
+            Counter malcnt = context.getCounter("mygroup", "counter");
+            malcnt.increment(1);
+            System.err.printf("splitted clauses num %d < 15:%s\n", clause.length, line);
+            return;
+        }
+
+        word.set("game", clause[0]); // game name
+        context.write(word, one); 
+        word.set("version", clause[1]); // game version
+        context.write(word, one); 
+        word.set("channel", clause[2]); // channel
+        context.write(word, one); 
+        word.set("device", clause[4]); // device type
+        context.write(word, one); 
+        word.set("OS", clause[5]); // OS type
+        context.write(word, one); 
+        word.set("firmversion", clause[6]); // firmware version
+        context.write(word, one); 
+        word.set("resolution", clause[7]); // resolution
+        context.write(word, one); 
+        word.set("carrier", clause[8]); // carrier
+        context.write(word, one); 
+        word.set("lang", clause[10]); // language
+        context.write(word, one); 
+        word.set("region", clause[11]); // region
+        context.write(word, one); 
+        word.set("event", clause[14]); // event type
+        context.write(word, one); 
+    }
+ } 
+
+ public static class Reduce extends Reducer<TextPair, IntWritable, TextPair, IntWritable> 
+ {
+    public void reduce(TextPair key, Iterable<IntWritable> values, Context context) 
+      throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        context.write(key, new IntWritable(sum));
+    }
+ }
+        
+ public int run(String[] args) throws Exception {
+    Job job = new Job(getConf(), "Wireless-UniqClause");
+    job.setJarByClass(GetUniqClause.class);
+    //job.setNumMapTasks(1);
+    //job.setNumReduceTasks(1);
+
+    job.setMapperClass(Map.class);
+    job.setCombinerClass(Reduce.class);
+    job.setReducerClass(Reduce.class);
+
+    // set Partion and Group
+    job.setPartitionerClass(TextPairPartitioner.class);
+    job.setGroupingComparatorClass(TextPairComparator.class);
+
+    // the map output is TextPair, IntWritable
+    job.setMapOutputKeyClass(TextPair.class);
+    job.setMapOutputValueClass(IntWritable.class);
+
+    // the reduce output is TextPair, IntWritable
+    job.setOutputKeyClass(TextPair.class);
+    job.setOutputValueClass(IntWritable.class);
+        
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+    TextInputFormat.addInputPath(job, new Path(args[0]));
+    TextOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+    job.waitForCompletion(true);
+    return 0;
+ }
+
+ public static void main(String args[]) throws Exception
+ {
+     if (args.length != 4) {
+         System.err.println("Usage: GetUniqClause <input-dir> <out-dir> -<m|d|b> <db-options>\n" + 
+                 "\t-m\tonly do map reduce operation\n" + 
+                 "\t-d\tonly do database operation\n" +
+                 "\t-b\tdo both mapred and database operation\n\n" + 
+                 "\tdb options example:\n\tuser:password@ip[:port]/database"
+                 );
+         System.exit(-1);
+     }
+
+     boolean do_mapred = false;
+     boolean do_db = false;
+     if (args[2].equals("-m")) { do_mapred = true; } 
+     if (args[2].equals("-d")) { do_db = true; } 
+     if (args[2].equals("-b")) {
+         do_mapred = true;
+         do_db = true;
+     } 
+
+     int ret = 0;
+     if (do_mapred) {
+         ToolRunner.run(new Configuration(), new GetUniqClause(), args);
+         System.err.printf("\nmapred end with code: %d\n", ret);
+         if (ret != 0) {
+             System.exit(ret);
+         }
+     }
+
+     if (do_db) {
+         System.out.println("load unique phrase into database table\n");
+         ret = buildEnv.setup_env(args[1], args[3]);
+         System.err.printf("\ndatabase load end with code: %d\n", ret);
+         System.exit(ret);
+     }
+ }
+}
--- hadoop/tools/hdp-push/sync_run.sh
+++ hadoop/tools/hdp-push/sync_run.sh
@@ -1,15 +1,28 @@
 #! /bin/bash
 
+ENV_FILE="~/.environment"
+if [[ -f $ENV_FILE ]]; then
+    source $ENV_FILE
+fi
 source comm_func.sh
 
-LAST_DAYS=1
+SRC_DIR_PARENT=/home/lc/hadoop/wireless/data/
+if [[ ! -d $SRC_DIR_PARENT ]]; then
+    echo "Source Directory not exist: $SRC_DIR_PARENT";
+    echo; exit;
+fi
+LAST_DAYS=7
 TIME_TOKEN="-mtime -$LAST_DAYS"
-SRC_DIR_PARENT=~/hadoop/data/
 SRC_DIR_PARENT=`cd $SRC_DIR_PARENT && pwd`
 SRC_DIR_LEN=`expr length $SRC_DIR_PARENT`
 DST_DIR_PARENT=/user/lc
 SYNC_CMD='bin/hadoop hdp_push'
-DO_CMD='wo lie ge qu'
+#BEG_CMD='bin/haddoop jar GetUniqClause.jar GetUniqClause'
+BEG_CMD='false'
+DO_CMD='wo qu'
+INPUT_PARENT='/wireless'
+OUTPUT_PARENT='/output/wireless'
+DB_OPTS='root:ta0mee@10.1.1.60/db_wireless_dim'
 
 if [[ $1 == '-h' ]]; then
     echo "usage: `basename $0` [ n days ago ]"
@@ -21,6 +34,7 @@ elif [[ $1 ]]; then
     LAST_DAYS=$1
 fi
 
+echo "merge process: start at `date`"
 FILES=`find $SRC_DIR_PARENT $TIME_TOKEN`
 # merge all found files
 for f in $FILES; do
@@ -39,18 +53,36 @@ for f in $FILES; do
     fi
 done
 
+echo "mapred process: start at `date`"
 DAYS=`find $SRC_DIR_PARENT -maxdepth 1 $TIME_TOKEN`
 for d in $DAYS; do
     day_time=`get_day_time $d`
     if [[ $day_time == "" ]]; then
         continue
     else
+        # prepare environment
+        beg_cmd_str="$BEG_CMD $INPUT_PARENT/$d $OUTPUT_PARENT/$d -b $DB_OPTS"
+        if [[ $VERBOSE ]]; then
+            echo $beg_cmd_str; 
+        fi
+        eval $beg_cmd_str
+        ECODE=$?
+        if [[ $ECODE != 0 ]]; then
+            echo "error detected, code: $ECODE";    exit $ECODE
+        fi
+
+        # do real mapred task
         job_cmd_str="$DO_CMD $day_time"
         if [[ $VERBOSE ]]; then
             echo $job_cmd_str; echo
         fi
         eval $job_cmd_str
+        ECODE=$?
+        if [[ $ECODE != 0 ]]; then
+            echo "error detected, code: $ECODE";    exit $ECODE
+        fi
     fi
 done
 
+echo "all process: end at `date`"
 echo
