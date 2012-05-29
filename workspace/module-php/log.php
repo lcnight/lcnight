a0cdbc36e5cd521cb7b4eae748eb77831c2728a8
lc <lcnight2012@gmail.com>
2012-05-29 13:54:52 +0800

    add php modules

---
+++ workspace/module-php/log.php
@@ -0,0 +1,108 @@
+<?php 
+if(!defined('nl')) define('nl', "\n");
+
+// 默认在当前目录下有个logs的目录存放日志文件。
+// 要是定制日志的存放目录和前缀，必须先调用initialize初始化日志模块，否则可能会出错。
+
+class LOG {
+    private static $m_init = false;
+    private static $m_logdir = './logs/';
+    private static $m_logpre = 'mylog-';
+    private static $m_logdst = array('console'); // can be 'file' | 'console'
+    private static $m_logprocs = array(); // array('file' => 'log_file', 'console'=>'log_console');
+    private static $m_logfiles = array('Debug' => null, 'Info' => null, 'Warn' => null, 'Error' => null);
+    private static $m_fd = null;
+
+    public static function initialize($logdst = array('console'), $logpre = 'mylog-', $logdir = './logs/') {
+        self::$m_logdst = $logdst;
+
+        if(!file_exists($logdir)) mkdir($logdir);
+        self::$m_logdir = $logdir;
+        self::$m_logpre = $logpre;
+
+        foreach($logdst as $dst) {
+            switch ($dst) {
+            case 'file': 
+                self::$m_logprocs['file'] = 'log_file';
+                break;
+            case 'console':
+                self::$m_logprocs['console'] = 'log_console';
+                break;
+            default:
+                fprintf(STDERR, 'supply not supported log destination'.nl);
+                exit(-1);
+            }
+        }
+        return self::$m_init = true;
+    }
+
+    public static function OK() {
+        if (self::$m_init == false) {
+            return false;
+        }
+    } 
+
+    public static function open_file($token = 'Debug') 
+    {
+        if (self::$m_logfiles[$token] == null) {
+            self::$m_logfiles[$token] = 
+                fopen(self::$m_logdir . '/' . self::$m_logpre  . "$token.txt" , 'a+');
+        }
+
+        return self::$m_logfiles[$token];
+    }
+
+    public static function do_log(&$fd, $token = 'Debug', $msg = '')
+    {
+        if (!self::OK() && !self::initialize()) {
+            fprintf(STDERR, 'fail to initialize'.nl);
+            return ;
+        }
+
+        if ($msg == '') {
+            $logmsg = nl;
+        } else {
+            $logmsg = '['.date('Y-m-d H:i:s').'] '. readlink('/proc/self') .' ['.$token.'] ' . $msg . nl;;
+        }
+
+        //var_dump(self::$m_logprocs);
+        foreach(self::$m_logprocs as $dst => $proc) {
+            $proc($fd, $logmsg);
+        }
+    } 
+
+    public static function Error($msg = '') 
+    {
+        $fd = self::open_file('Error');
+        self::do_log($fd, 'Error', $msg);
+    }
+
+    public static function Warn($msg = '') 
+    {
+        $fd = self::open_file('Warn');
+        self::do_log($fd, 'Warn', $msg);
+    }
+
+    public static function Info($msg = '') 
+    {
+        $fd = self::open_file('Info');
+        self::do_log($fd, 'Info', $msg);
+    }
+
+    public static function Debug($msg = '') 
+    {
+        $fd = self::open_file('Debug');
+        self::do_log($fd, 'Debug', $msg);
+    }
+
+}
+
+function log_file(&$fd, &$msg) {
+    fwrite($fd, $msg);
+}
+
+function log_console(&$fd, &$msg) {
+    fprintf(STDOUT, $msg);
+}
+
+?>
---
+++ workspace/module-php/util.php
@@ -0,0 +1,104 @@
+<?php 
+// define new line in environment
+define(nl, "\n");
+// run in DEBUG/RELEASE mode
+define(DEBUG, true);
+// comma separated email receiptor list
+define(Recip, 'lc@taomee.com');
+
+function Alert($msg) {
+    if ($msg == '') { 
+        echo nl; return;
+    }
+    echo date('Y-m-d H:i:s')." [Alert] ".$msg.nl;
+}
+function Msg($msg) {
+    if ($msg == '') { 
+        echo nl; return;
+    }
+    echo date('Y-m-d H:i:s')." [Info] ".$msg.nl;
+}
+function Debug($msg) {
+    if ($msg == '') { 
+        echo nl; return;
+    }
+    if(DEBUG) echo date('Y-m-d H:i:s')." [Debug] ".$msg.nl;
+}
+
+function SendMail($title, $content)
+{
+    $ll = system("echo \"$content\"| mail -s \"$title\" " . Recip, $retval);
+    Msg("return code: $retval($ll)\nsend mail\ntitle: ${title}\nto: ".Recip);
+}
+
+function get_dirfilepat($dir, $pat, &$filesarr) 
+{
+    $dh  = opendir($dir);
+    while ($filename = readdir($dh)) {
+        if (stristr($filename, $pat)) {
+            $filename = "$dir/$filename";
+            if (file_used($filename, $pids)) {
+                Msg("$filename used by: $pids");
+                continue;
+            }
+            $filesarr[] = $filename;
+        }
+    }
+    closedir($dh);
+    sort($filesarr);
+}
+
+function file_used(&$file, &$pids='')
+{
+    $cmd = "fuser $file";
+    $ret_str = system($cmd, $ret_code);
+    // 0 : used
+    // 1 : none
+    // >1: fatal error
+    if ($ret_code > 1) {
+        Msg("System error code: $ret_code, \n\t command: $cmd\n");
+        exit($ret_code);
+    }
+
+    if (strlen($ret_str) == 0) {
+        return false;
+    } else {
+        $pids = $ret_str;
+        return true;
+    }
+}
+
+function get_num($str) 
+{
+    $value = 0;
+    for ($i = 0; $str[$i] ; ++$i) {
+        if (is_numeric($str[$i])) {
+            $numstr = substr($str, $i);
+            $value = intval($numstr);
+            break;
+        }
+    }
+    return $value;
+}
+
+function bak_file($bak_dir, $bak_flag, $file) 
+{
+    //Msg("dir: $bak_dir\n, flag: $bak_flag\n, file: $file");
+    if (!file_exists($bak_dir)) { mkdir($bak_dir); }
+
+    $rel_bak_dir = $bak_dir . '/' . $bak_flag;
+    if (!file_exists($rel_bak_dir)) { mkdir($rel_bak_dir); }
+
+    $basefname = basename($file);
+    Msg("rename($file, $rel_bak_dir/$basefname)");
+    return rename($file, $rel_bak_dir.'/'.$basefname);
+}
+
+function purge_dir($t_dir, $days) 
+{
+    $cmd = "find $t_dir -maxdepth 1 -atime +$days -exec rm -rfv {} +;";
+    system($cmd, $ret_val);
+    return $ret_val;
+}
+
+?>
