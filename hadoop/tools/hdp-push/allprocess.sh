8eb603bea93043b5e2b4ec29fb8b70c400433a45
lc <lcnight2012@gmail.com>
2012-05-31 17:47:29 +0800

    change run script and hadoop configuration reference

--- hadoop/tools/hdp-push/allprocess.sh
+++ hadoop/tools/hdp-push/allprocess.sh
@@ -12,12 +12,12 @@ usage ()
 
 do_sync=0
 do_mapr=0
-do_week=0
+# do_week=0
 while getopts smwh opt; do
 	case $opt in 
 		s) do_sync=1;;
 		m) do_mapr=1;;
-		w) do_week=1;;
+#		w) do_week=1;;
 		h) usage;;
 		*) usage;;
 	esac
@@ -30,22 +30,32 @@ if [[ $do_sync == 1 ]]; then
 	echo "[`date '+%F %T'`] file sync complete "
 fi
 
+####./sync_mapr.sh -m -B 30 -E 21 >>./logs/mapr30.log 2>&1 &
+####./sync_mapr.sh -m -B 21 -E 14 >>./logs/mapr21.log 2>&1 &
+####./sync_mapr.sh -m -B 14 -E 7 >>./logs/mapr14.log 2>&1 &
 if [[ $do_mapr == 1 ]]; then
+    ./sync_mapr.sh -m -S ./map-reduce-pig/calc_active.sh >>./logs/mapr_active.log 2>&1
+	echo "[`date '+%F %T'`] run basic mapreduce success !"
+
 	# range [x, y)
-	./sync_mapr.sh -m -B 30 -E 21 >>./logs/mapr30.log 2>&1 &
-	./sync_mapr.sh -m -B 21 -E 14 >>./logs/mapr21.log 2>&1 &
-	./sync_mapr.sh -m -B 14 -E 7 >>./logs/mapr14.log 2>&1 &
-	./sync_mapr.sh -m -B 7 -E 0 >>./logs/mapr7.log 2>&1 &
+    ./sync_mapr.sh -m -D -B 30 -E 20 ./map-reduce-pig/calc_other.sh >>./logs/mapr_oth30.log 2>&1 &
+    ./sync_mapr.sh -m -D -B 20 -E 10 ./map-reduce-pig/calc_other.sh >>./logs/mapr_oth20.log 2>&1 &
+    ./sync_mapr.sh -m -D -B 10 -E 0 ./map-reduce-pig/calc_other.sh >>./logs/mapr_oth10.log 2>&1 &
 
 	# wait all child processes to complete
 	wait
-	echo "[`date '+%F %T'`] days mapreduce complete "
+	echo "[`date '+%F %T'`] common mapreduce complete, do slow calculate"
+
+    ./sync_mapr.sh -m -D -B 30 -E 20 ./map-reduce-pig/calc_slow.sh >>./logs/mapr_slow30.log 2>&1 &
+    ./sync_mapr.sh -m -D -B 20 -E 10 ./map-reduce-pig/calc_slow.sh >>./logs/mapr_slow20.log 2>&1 &
+    ./sync_mapr.sh -m -D -B 10 -E 0 ./map-reduce-pig/calc_slow.sh >>./logs/mapr_slow10.log 2>&1 &
+	wait
 fi
 
-if [[ $do_week == 1 ]]; then
-	./sync_mapr.sh -w >>./logs/mapr_weekmonth.log 2>&1 
-	echo "[`date '+%F %T'`] week month mapreduce complete "
-fi
+##if [[ $do_week == 1 ]]; then
+##	./sync_mapr.sh -w >>./logs/mapr_weekmonth.log 2>&1 
+##	echo "[`date '+%F %T'`] week month mapreduce complete "
+##fi
 
 echo "[`date '+%F %T'`] all done"
 echo
--- hadoop/tools/hdp-push/sync_mapr.sh
+++ hadoop/tools/hdp-push/sync_mapr.sh
@@ -8,50 +8,52 @@ source comm_func.sh
 #################################### Variables ##########################################
 VERBOSE=1
 LAST_DAYS=1
-SYNC_CMD='hadoop hdp_push'
-UNIQ_CMD='hadoop jar GetUniqClause.jar GetUniqClause'
-#MAPR_CMD='bash calc.sh '
+# MAPR_CMD='bash map-reduce-pig/calc.sh '
+# WEEKMONTH_CMD='bash map-reduce-pig/calc_week_month.sh '
 
-MAPR_CMD='bash map-reduce-pig/calc.sh '
-WEEKMONTH_CMD='bash map-reduce-pig/calc_week_month.sh '
 # database connection option
 DB_OPTION='hadoop:HA#2jsOw%x@192.168.71.45/db_wireless_dim'
 # local source dir
 SRC_DIR_PARENT=/opt/stat-log/data/1000000
 # hadoop destination dir
 DST_DIR_PARENT=/wireless/1000000
+# tmp dir used for intermediate result
+HADOOP_TMP='/wireless/tmp'
 if [[ ! -d $SRC_DIR_PARENT ]]; then
     echo "Not Exist Source Dir: $SRC_DIR_PARENT";
     echo;
 fi
 
-# tmp dir used for intermediate result
-HADOOP_TMP='/wireless/tmp'
+dryrun=0
 doEnv=0
 doMapr=0
 MapRangeBeg=30
 MapRangeEnd=0
-doweekmonth=0
+modeSeq=0
+modeDiv=1
+
 #################################### Functions ##########################################
 Usage() {
-    echo -e "Usage: `basename $0` [opt]\n"\
-    "    opt can be as following:\n"\
+    echo -e "Usage: `basename $0` [opt] <running script>\n"\
+    "opt can be as following:\n"\
     "     -t        sync latest [$LAST_DAYS] days change from Local to Hadoop\n"\
     "     -s        local source data dir [$SRC_DIR_PARENT]\n"\
     "     -d        hadoop destination data dir [$DST_DIR_PARENT]\n"\
     "     -b        database connection option [$DB_OPTION]\n"\
 	"     -e        do build environment (merge files/build unique phrase), default [$doEnv]\n"\
 	"     -m        do map reduce day's data related, default [$doMapr]\n"\
-	"     -B        do mapreduce from [$MapRangeBeg] days ago\n"\
-    "     -E        do mapreduce end at [$MapRangeEnd] days ago\n"\
-	"     -w        do week and month aggregate [$doweekmonth]\n"\
+	"     -S        do map in Sequnce Mode [$modeSeq], not compatiable with Divided Mode\n"\
+	"     -D        do map in Divided Mode [$modeDiv], not compatiable with Sequnce Mode\n"\
+	"     -B        do Divided Mode mapreduce from [$MapRangeBeg] days ago\n"\
+    "     -E        do Divided Mode mapreduce end at [$MapRangeEnd] days ago\n"\
     "     -v        run in verbose mode\n"\
     "     -q        run in quiet mode\n"\
+	"     -z        dryrun mode [$dryrun]\n"\
     "     -h        show usage\n"
     exit 2;
 }
 
-while getopts hvqt:s:d:b:B:E:emw opt
+while getopts hvemzqt:s:d:b:SDB:E: opt
 do
     case $opt in
         v) VERBOSE=1;;
@@ -60,18 +62,25 @@ do
         s) SRC_DIR_PARENT=$OPTARG;;
         d) DST_DIR_PARENT=$OPTARG;;
         b) DB_OPTION=$OPTARG;;
-        B) MapRangeBeg=$OPTARG;;
-        E) MapRangeEnd=$OPTARG;;
 		e) doEnv=1;;
 		m) doMapr=1;;
-		w) doweekmonth=1;;
+		S) modeSeq=1; modeDiv=0;;
+		D) modeDiv=1; modeSeq=0;;
+        B) MapRangeBeg=$OPTARG;;
+        E) MapRangeEnd=$OPTARG;;
+		z) dryrun=1;;
         h) Usage ;;   
         ?) Usage ;;
     esac
 done
-# shift $(($OPTIND -1))
-# if [[ $* ]]; then fi
+if [[ ! ${!OPTIND} ]]; then
+	Info 'Must supply one running script'"\n"
+	Usage;
+fi
+runningShell="bash ${!OPTIND} "
 
+SYNC_CMD='hadoop hdp_push'
+UNIQ_CMD='hadoop jar GetUniqClause.jar GetUniqClause'
 
 SRC_DIR_PARENT=`cd $SRC_DIR_PARENT && pwd`
 SRC_DIR_LEN=`expr length $SRC_DIR_PARENT`
@@ -80,15 +89,16 @@ TIME_TOKEN="-mtime -$LAST_DAYS"
 #################################### Main Process ##########################################
 ############################################################################################
 echo
-Info 'environment test'
+Info '#################################### environment ####################################'
 Info "CLASSPATH: $CLASSPATH"
 Info "HADOOP_CLASSPATH: $HADOOP_CLASSPATH"
+Info '#################################### environment ####################################'
 if [[ $doEnv == 1 ]]; then
     # merge all found files
     Info 'start merge process'
     comma_files=''
 
-	batchNum=100
+	batchNum=300
 	batchSrc=''
 	batchDst=''
 	curNum=0
@@ -114,97 +124,113 @@ if [[ $doEnv == 1 ]]; then
 				batchDst=${batchDst/%,/}
 				sync_cmd_str="$SYNC_CMD -m $batchSrc -f $batchDst"
 				Debug $sync_cmd_str; 
-				eval $sync_cmd_str
-				CheckResult $? 'ERROR: Sync file to hadoop' "running command:\n$sync_cmd_str"
+				if [[ $dryrun == 0 ]]; then
+					eval $sync_cmd_str
+					CheckResult $? 'ERROR: Sync file to hadoop' "running command:\n$sync_cmd_str"
+				fi
 				batchSrc=''
 				batchDst=''
 				curNum=0;
-				exit
 			fi
         else
             Info "ignore file: $f"
         fi
     done
-	Info 'end files merge'
-
-	exit
+	if [[ $curNum -gt 0 ]]; then
+		sync_cmd_str="$SYNC_CMD -m $batchSrc -f $batchDst"
+		Debug $sync_cmd_str; 
+		if [[ $dryrun == 0 ]]; then
+			eval $sync_cmd_str
+		fi
+	fi
+	Info 'end merge files'
 
     # get uniq prhase from changed files and store into db
     Info 'start unique phrase'
     comma_files=${comma_files/%,/}
-
 	if [[ $comma_files == '' ]]; then
 		Info 'no change files'
 		exit
 	fi
     uniq_cmdstr="$UNIQ_CMD '$comma_files' '$HADOOP_TMP/phrase/`date '+%Y%m%d'`-$$-$RANDOM' -b '$DB_OPTION'"
     Debug $uniq_cmdstr
-    eval $uniq_cmdstr
-    # CheckResult $? 'ERROR: prepare mapr env' "running command:\n$uniq_cmdstr"; 
-    Info 'end build environment'
-
-#   if [[ $onlyEnv == 1 ]];then
-#       Info 'configure to ***only*** run build environment'
-#   fi
+	if [[ $dryrun == 0 ]]; then
+		eval $uniq_cmdstr
+		CheckResult $? 'ERROR: prepare mapr env' "running command:\n$uniq_cmdstr"; 
+	fi
+    Info 'End build environment'
 fi
 
-rangeBeg=`date -d "$MapRangeBeg days ago" '+%Y%m%d'`
-rangeEnd=`date -d "$MapRangeEnd days ago" '+%Y%m%d'`
 # do map reduce process for modified days
 if [[ $doMapr == 1 ]]; then
-    Info 'start mapred process'
-    # DAYS=`find $SRC_DIR_PARENT -maxdepth 1 $TIME_TOKEN | sort`
-    DAYS=`find $SRC_DIR_PARENT $TIME_TOKEN | sed -n '/\/\(.*\)\/.*/s//\1/p' | sort | uniq`
-	Debug "Need Process: $DAYS"
-    for d in $DAYS; do
-        day_time=`get_day_time $d`
-        if [[ $day_time == "" ]]; then
-            continue
+	if [[ $modeSeq == 1 && $modeDiv == 1 ]]; then
+		Usage;
+	fi
+
+	Info 'start mapred process'
+	DAYS=`find $SRC_DIR_PARENT $TIME_TOKEN | sed -n '/\/\(.*\)\/.*/s//\1/p' | sort | uniq`
+	Debug "Need Process: `echo $DAYS | tr '\n' ' '`"
+
+	if [[ $modeDiv == 1 ]] ; then
+		rangeBeg=`date -d "$MapRangeBeg days ago" '+%Y%m%d'`
+		rangeEnd=`date -d "$MapRangeEnd days ago" '+%Y%m%d'`
+	elif [[ $modeSeq == 1 ]]; then
+		startDay=20120326
+		cutDay=`date -d '30 days ago' '+%Y%m%d'`
+		stopPoint=$(($startDay<$cutDay?$cutDay:$startDay))
+		curDay=`date '+%Y%m%d'`
+
+		rangeBeg=$stopPoint
+		rangeEnd=$curDay
+	else
+		Info "Must run map reduce in [Seq|Div] mode";
+		Usage;
+	fi
+
+	for d in $DAYS; do
+		day_time=`get_day_time $d`
+		if [[ $day_time == "" ]]; then
+			continue
 		elif [[ $day_time -lt $rangeBeg || $day_time -gt $rangeEnd || $day_time == $rangeEnd ]]; then
 			Info "$day_time not in range [$rangeBeg, $rangeEnd)"
 			continue
 		else
-            # do real mapred task
-            job_cmd_str="$MAPR_CMD $day_time"
-            Debug $job_cmd_str
-			eval $job_cmd_str
-            CheckResult $? "ERROR: run mapr job" "running command:\n$job_cmd_str"
-        fi
-    done
-
-#   if [[ $onlyMapr == 1 ]];then
-#       Info 'configure to ***only*** run MapRed'
-#   fi
-fi
-
-startDay=20120326
-cutDay=`date -d '30 days ago' '+%Y%m%d'`
-stopPoint=$(($startDay<$cutDay?$cutDay:$startDay))
-curDay=`date '+%Y%m%d'`
-modDaysStart=$curDay
-modDaysEnd=$curDay
-if [[ $doweekmonth == 1 ]]; then
-    DAYS=`find $SRC_DIR_PARENT $TIME_TOKEN | sed -n '/\/\(.*\)\/.*/s//\1/p' | sort | uniq`
-    for d in $DAYS; do
-        day_time=`get_day_time $d`
-        if [[ $day_time == "" ]]; then
-            continue
-		elif [[ $day_time -lt $stopPoint || $day_time -gt $curDay ]]; then
-			Info "$day_time not in range [$stopPoint, $curDay]"
-			continue
-		else
-			if [[ $day_time -lt $modDaysStart ]]; then modDaysStart=$day_time; fi
-			if [[ $day_time -gt $modDaysEnd ]]; then modDaysEnd=$day_time; fi
+			job_cmd_str="$runningShell $day_time"
+			Debug $job_cmd_str
+			if [[ $dryrun == 0 ]]; then
+				# do real mapred task
+				eval $job_cmd_str
+				CheckResult $? "ERROR: run mapr job" "running command:\n$job_cmd_str"
+			fi
 		fi
-    done
-
-	# tune: run weeks & months operation
-	weekmon_cmdstr="$WEEKMONTH_CMD $modDaysStart $modDaysEnd"
-	Debug "Week Month processing ..."
-	Debug "$weekmon_cmdstr"
-	eval $weekmon_cmdstr
+	done
 fi
-
-
 Info "all done"
-echo
+
+####startDay=20120326
+####cutDay=`date -d '30 days ago' '+%Y%m%d'`
+####stopPoint=$(($startDay<$cutDay?$cutDay:$startDay))
+####curDay=`date '+%Y%m%d'`
+####modDaysStart=$curDay
+####modDaysEnd=$curDay
+####if [[ $doweekmonth == 1 ]]; then
+####    DAYS=`find $SRC_DIR_PARENT $TIME_TOKEN | sed -n '/\/\(.*\)\/.*/s//\1/p' | sort | uniq`
+####    for d in $DAYS; do
+####        day_time=`get_day_time $d`
+####        if [[ $day_time == "" ]]; then
+####            continue
+####		elif [[ $day_time -lt $stopPoint || $day_time -gt $curDay ]]; then
+####			Info "$day_time not in range [$stopPoint, $curDay]"
+####			continue
+####		else
+####			if [[ $day_time -lt $modDaysStart ]]; then modDaysStart=$day_time; fi
+####			if [[ $day_time -gt $modDaysEnd ]]; then modDaysEnd=$day_time; fi
+####		fi
+####    done
+####
+####	# tune: run weeks & months operation
+####	weekmon_cmdstr="$WEEKMONTH_CMD $modDaysStart $modDaysEnd"
+####	Debug "Week Month processing ..."
+####	Debug "$weekmon_cmdstr"
+####	eval $weekmon_cmdstr
+####fi
